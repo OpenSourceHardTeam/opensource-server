fcaa84da9780c82ffed68301619f6cb5679ff77c
Joon9750 <junhyeok2166@daum.net>
2025-03-25 16:58:10 +0900

    [Add] BaseTimeEntity 구현

--- module-application/src/main/java/org/opensource/BookingApplication.java
+++ module-application/src/main/java/org/opensource/BookingApplication.java
@@ -2,8 +2,11 @@ package org.opensource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookingApplication {
     public static void main(String[] args) {
 
