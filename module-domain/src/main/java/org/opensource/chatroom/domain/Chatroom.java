ae1b9d124d1cb81aa9ef25be4abe61ecf54c592c
Joon9750 <junhyeok2166@daum.net>
2025-03-25 16:13:36 +0900

    [Add] Chatroom 폴더 구조 설정

--- module-domain/src/main/java/org/opensource/chatroom/domain/Chatroom.java
+++ module-domain/src/main/java/org/opensource/chatroom/domain/Chatroom.java
@@ -1,4 +1,22 @@
 package org.opensource.chatroom.domain;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class Chatroom {
+    private long id;
+    private String externalRoomId;
+    private String name;
+    private ChatroomType type;
+    private String topic;
+    private Long bookId;
+    private int maxParticipants;
+    private String password;
+    private LocalDateTime expiryDate;
+    private long messageSequence;
+    private int currentParticipants;
+    private int messageCount;
+    private boolean isArchived;
 }
