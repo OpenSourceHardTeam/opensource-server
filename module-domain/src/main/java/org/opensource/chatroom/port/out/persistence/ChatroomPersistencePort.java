f4d55776c9c45e9c737750015e2470fbe4f73311
Joon9750 <junhyeok2166@daum.net>
2025-03-27 22:37:46 +0900

    [Add] ChatroomRespository 구현

--- module-domain/src/main/java/org/opensource/chatroom/port/out/persistence/ChatroomPersistencePort.java
+++ module-domain/src/main/java/org/opensource/chatroom/port/out/persistence/ChatroomPersistencePort.java
@@ -1,4 +1,11 @@
 package org.opensource.chatroom.port.out.persistence;
 
+import org.opensource.chatroom.domain.Chatroom;
+
+import java.util.Optional;
+
 public interface ChatroomPersistencePort {
+
+    Optional<Chatroom> findById(Long id);
+    void deleteById(Long id);
 }
--- module-infra/persistence-database/src/main/java/org/opensource/chatroom/repository/ChatroomRespository.java
+++ module-infra/persistence-database/src/main/java/org/opensource/chatroom/repository/ChatroomRespository.java
@@ -1,4 +1,31 @@
 package org.opensource.chatroom.repository;
 
-public class ChatroomRespository {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.opensource.chatroom.domain.Chatroom;
+import org.opensource.chatroom.entity.ChatroomEntity;
+import org.opensource.chatroom.port.out.persistence.ChatroomPersistencePort;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ChatroomRespository implements ChatroomPersistencePort {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private final ChatroomJpaRepository chatroomJpaRepository;
+
+    @Override
+    public Optional<Chatroom> findById(Long id) {
+        return chatroomJpaRepository.findById(id).map(ChatroomEntity::toModel);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        chatroomJpaRepository.deleteById(id);
+    }
 }
