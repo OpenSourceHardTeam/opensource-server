5e7f42c506453380cf4ce6dc21dc0c0db1ec94d6
SSUN <rlatjsghks4647@naver.com>
2025-03-27 22:17:48 +0900

    [ADD#21] user domain 구현

---
+++ module-domain/src/main/java/org/opensource/user/domain/UserCredentials.java
@@ -0,0 +1,27 @@
+package org.opensource.user.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserCredentials {
+    private long id;
+    private User user;
+    private String password;
+    private LoginType loginType;
+    private String refreshToken;
+
+    @Builder
+    public UserCredentials(
+            long id,
+            User user,
+            String password,
+            LoginType loginType,
+            String refreshToken) {
+        this.id = id;
+        this.user = user;
+        this.password = password;
+        this.loginType = loginType;
+        this.refreshToken = refreshToken;
+    }
+}
