e4e1a7bb59100f3bbe3d1b638f6a253dc21abcc6
SSUN <rlatjsghks4647@naver.com>
2025-03-30 02:50:16 +0900

    [ADD] error type 추가 #34

---
+++ module-common/src/main/java/type/user/UserErrorType.java
@@ -0,0 +1,29 @@
+package type.user;
+
+import type.ErrorType;
+
+public enum UserErrorType implements ErrorType {
+    CHECK_EMAIL_VALIDATION_ERROR(401, "이메일 인증에 실패하였습니다."),
+    SEND_EMAIL_ERROR(500, "이메일 전송에 실패하였습니다. 다시 시도해주세요."),
+    EMAIL_AUTH_TIMEOUT(401, "인증 시간을 초과하였습니다. 다시 시도해주세요"),
+    EMAIL_AUTH_FAIL(401, "인증번호가 일치하지 않습니다.")
+    ;
+
+    private final int code;
+    private final String message;
+
+    UserErrorType(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
