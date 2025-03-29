b2926a85069c965cc628430fa35a116dd3e114bd
SSUN <rlatjsghks4647@naver.com>
2025-03-30 03:32:02 +0900

    [FIX] UserErrorType status code 수정 #31

--- module-common/src/main/java/type/user/UserErrorType.java
+++ module-common/src/main/java/type/user/UserErrorType.java
@@ -3,10 +3,9 @@ package type.user;
 import type.ErrorType;
 
 public enum UserErrorType implements ErrorType {
-    CHECK_EMAIL_VALIDATION_ERROR(401, "이메일 인증에 실패하였습니다."),
+    CHECK_EMAIL_VALIDATION_ERROR(401, "인증번호가 일치하지 않습니다."),
     SEND_EMAIL_ERROR(500, "이메일 전송에 실패하였습니다. 다시 시도해주세요."),
-    EMAIL_AUTH_TIMEOUT(401, "인증 시간을 초과하였습니다. 다시 시도해주세요"),
-    EMAIL_AUTH_FAIL(401, "인증번호가 일치하지 않습니다.")
+    EMAIL_AUTH_TIMEOUT_ERROR(400, "인증 시간을 초과하였습니다. 다시 시도해주세요")
     ;
 
     private final int code;
