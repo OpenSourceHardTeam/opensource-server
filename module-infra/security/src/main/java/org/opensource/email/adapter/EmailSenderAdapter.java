95f521ddbf5cc9055a4d357b03ee57da5e65fdfc
SSUN <rlatjsghks4647@naver.com>
2025-03-30 03:38:17 +0900

    [ADD] EmailSenderAdapter 구현 #31

---
+++ module-infra/security/src/main/java/org/opensource/email/adapter/EmailSenderAdapter.java
@@ -0,0 +1,43 @@
+package org.opensource.email.adapter;
+
+import exception.InternalServerException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.opensource.user.port.out.EmailSenderPort;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import type.user.UserErrorType;
+
+@Component
+@RequiredArgsConstructor
+public class EmailSenderAdapter implements EmailSenderPort {
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    @Override
+    public void sendEmail(String email, String authCode) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            message.setFrom(senderEmail);
+            message.setRecipients(MimeMessage.RecipientType.TO, email);
+            message.setSubject("인증코드 입니다.");
+            message.setText(setContext(authCode), "utf-8", "html");
+
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            throw new InternalServerException(UserErrorType.SEND_EMAIL_ERROR);
+        }
+    }
+
+    private String setContext(String authCode) {
+        String body = "";
+        body += "<h4>" + "인증 코드를 입력하세요." + "</h4>";
+        body += "<h2>" + "[" + authCode + "]" + "</h2>";
+        return body;
+    }
+}
+
