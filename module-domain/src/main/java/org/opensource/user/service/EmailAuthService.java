3c92cd1cec1dbb23e44ca7e0623b5b2a3717a9d3
SSUN <rlatjsghks4647@naver.com>
2025-03-30 03:47:04 +0900

    [ADD] EmailAuthService 구현 #31

---
+++ module-domain/src/main/java/org/opensource/user/service/EmailAuthService.java
@@ -0,0 +1,62 @@
+package org.opensource.user.service;
+
+
+import exception.BadRequestException;
+import exception.InternalServerException;
+import exception.UnauthorizedException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.opensource.user.port.in.command.EmailAuthCommand;
+import org.opensource.user.port.in.usecase.EmailAuthUseCase;
+import org.opensource.user.port.out.persistence.EmailAuthPersistencePort;
+import org.opensource.user.port.out.EmailSenderPort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import type.user.UserErrorType;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EmailAuthService implements EmailAuthUseCase {
+    private final EmailAuthPersistencePort emailAuthPersistencePort;
+    private final EmailSenderPort emailSenderPort;
+
+    @Override
+    public void sendEmail(String email, long duration) {
+        if (emailAuthPersistencePort.existData(email)) {
+            emailAuthPersistencePort.deleteData(email);
+        }
+
+        try {
+            String authCode = createAuthCode();
+            EmailAuthCommand command = EmailAuthCommand.builder()
+                    .email(email)
+                    .authCode(authCode)
+                    .duration(duration)
+                    .build();
+            emailAuthPersistencePort.setData(command);
+            emailSenderPort.sendEmail(command.email(), authCode);
+        } catch (Exception e) {
+            throw new InternalServerException(UserErrorType.SEND_EMAIL_ERROR);
+        }
+    }
+
+    @Override
+    public void validateEmail(String email, String authCode) {
+        String storeCode = emailAuthPersistencePort.getData(email);
+        if (storeCode == null) {
+            throw new BadRequestException(UserErrorType.EMAIL_AUTH_TIMEOUT_ERROR);
+        }
+
+        if (!storeCode.equals(authCode)) {
+            throw new UnauthorizedException(UserErrorType.CHECK_EMAIL_VALIDATION_ERROR);
+        }
+    }
+
+    private String createAuthCode() {
+        return String.valueOf(ThreadLocalRandom.current().nextInt(100000, 1000000));
+    }
+}
