d052178229e359704d3bb5007c8fe551e2f989af
SSUN <rlatjsghks4647@naver.com>
2025-04-01 00:22:12 +0900

    [ADD] UserRepository 구현 #34

--- module-infra/persistence-database/src/main/java/org/opensource/user/repository/UserJpaRepository.java
+++ module-infra/persistence-database/src/main/java/org/opensource/user/repository/UserJpaRepository.java
@@ -1,7 +1,13 @@
 package org.opensource.user.repository;
 
+import org.opensource.user.domain.User;
 import org.opensource.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+    boolean existsByEmail(String email);
+
+    Boolean existsByName(String name);
+
+    User findByEmail(String email);
 }
