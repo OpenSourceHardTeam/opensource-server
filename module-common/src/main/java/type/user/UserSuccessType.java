cfa878d4c4871a2b741cf07beb9b813594424d5e
SSUN <rlatjsghks4647@naver.com>
2025-04-01 00:23:57 +0900

    [FEAT] UserSuccessType, UserErrorType 추가 #34

--- module-common/src/main/java/type/user/UserSuccessType.java
+++ module-common/src/main/java/type/user/UserSuccessType.java
@@ -4,7 +4,11 @@ import type.SuccessType;
 
 public enum UserSuccessType implements SuccessType {
     CHECK_EMAIL_VALIDATION_SUCCESS(200, "이메일 인증에 성공하였습니다."),
-    SEND_EMAIL_SUCCESS(200, "이메일을 성공적으로 전송하였습니다.");
+    SEND_EMAIL_SUCCESS(200, "이메일을 성공적으로 전송하였습니다."),
+    SIGN_UP_SUCCESS(200, "회원가입이 완료되었습니다."),
+    EMAIL_CAN_USE(200, "사용 가능한 이메일입니다."),
+    NAME_CAN_USE(200, "사용 가능한 이름입니다.")
+    ;
 
     private final int code;
     private final String message;
