131fcfed91510a6d396615cdad6b2de5367639f0
Joon9750 <junhyeok2166@daum.net>
2025-03-31 20:21:41 +0900

    [Add] UserChatroomApi 구현

---
+++ module-application/src/main/java/org/opensource/UserResponse.java
@@ -0,0 +1,4 @@
+package org.opensource;
+
+public class UserResponse {
+}
--- module-application/src/main/java/org/opensource/userchatroom/api/UserChatroomApi.java
+++ module-application/src/main/java/org/opensource/userchatroom/api/UserChatroomApi.java
@@ -1,6 +1,40 @@
 package org.opensource.userchatroom.api;
 
+import org.opensource.CommonApiResult;
+import org.opensource.UserResponse;
+import org.opensource.chatroom.dto.response.ChatroomResponse;
+import org.opensource.userchatroom.dto.request.JoinUserInChatroomRequest;
+import org.opensource.userchatroom.dto.response.JoinUserInChatroomResponse;
+import org.opensource.userchatroom.dto.response.UserChatroomResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface UserChatroomApi {
 
-//    joinUserInChatroom,
+    ResponseEntity<JoinUserInChatroomResponse> joinUserInChatroom(
+            @RequestBody JoinUserInChatroomRequest request);
+
+    ResponseEntity<UserChatroomResponse> getUserChatroom(
+            @PathVariable Long userId, @PathVariable Long chatroomId);
+
+    ResponseEntity<List<ChatroomResponse>> getUserChatrooms(
+            @PathVariable Long userId);
+
+    ResponseEntity<List<UserResponse>> getUsersInChatroom(
+            @PathVariable Long chatroomId);
+
+    ResponseEntity<CommonApiResult> deleteUserInChatroom(
+            @PathVariable Long userId, @PathVariable Long chatroomId);
+
+    ResponseEntity<CommonApiResult> deleteAllUsersInChatroom(
+            @PathVariable Long chatroomId);
+
+    ResponseEntity<CommonApiResult> deleteAllChatroomsThatUserIn(
+            @PathVariable Long userId);
+
+    ResponseEntity<Boolean> doesUserExistInChatroom(
+            @PathVariable Long userId, @PathVariable Long chatroomId);
 }
