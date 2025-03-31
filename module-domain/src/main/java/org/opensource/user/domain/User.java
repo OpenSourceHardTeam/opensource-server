48c2f8a99e707b719ff6aabfb4e54ec8fa397e9f
Joon9750 <junhyeok2166@daum.net>
2025-03-31 19:10:36 +0900

    [Add] Usecase에서 UserChatroomQueryUsecase로 분리

--- module-domain/src/main/java/org/opensource/user/domain/User.java
+++ module-domain/src/main/java/org/opensource/user/domain/User.java
@@ -11,15 +11,12 @@ import java.util.List;
 public class User {
     private Long id;
     private String name;
-    private List<UserChatroom> userChatrooms = new ArrayList<>();
 
     @Builder
     public User(
             Long id,
-            String name,
-            List<UserChatroom> userChatrooms) {
+            String name) {
         this.id = id;
         this.name = name;
-        this.userChatrooms = userChatrooms;
     }
 }
