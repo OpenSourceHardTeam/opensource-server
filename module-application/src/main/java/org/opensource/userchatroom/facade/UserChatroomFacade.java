7b16dc9251e06a4f8abe6c8a0fd473aff203a66b
Joon9750 <junhyeok2166@daum.net>
2025-03-31 20:04:40 +0900

    [Fix] UserChatroomUsecase joinUserInChatroom return 타입 변경

--- module-application/src/main/java/org/opensource/userchatroom/facade/UserChatroomFacade.java
+++ module-application/src/main/java/org/opensource/userchatroom/facade/UserChatroomFacade.java
@@ -1,4 +1,16 @@
 package org.opensource.userchatroom.facade;
 
+import lombok.RequiredArgsConstructor;
+import org.opensource.userchatroom.port.in.usecase.UserChatroomQueryUsecase;
+import org.opensource.userchatroom.port.in.usecase.UserChatroomUsecase;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserChatroomFacade {
+
+    private final UserChatroomQueryUsecase userChatroomQueryUsecase;
+    private final UserChatroomUsecase userChatroomUsecase;
+
+
 }
