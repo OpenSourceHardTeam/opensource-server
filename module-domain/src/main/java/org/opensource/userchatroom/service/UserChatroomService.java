e8cbc184cbf2449cd8d03e97ed22c0c2d6b10a11
Joon9750 <junhyeok2166@daum.net>
2025-04-01 17:10:38 +0900

    [Chore] 불필요 주석 제거

--- module-domain/src/main/java/org/opensource/userchatroom/service/UserChatroomService.java
+++ module-domain/src/main/java/org/opensource/userchatroom/service/UserChatroomService.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-//@Transactional
 @RequiredArgsConstructor
 public class UserChatroomService implements UserChatroomUsecase, UserChatroomQueryUsecase {
 
