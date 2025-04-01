d5fdac59c2c5b5e018aa9ee551d4e0f7fa237eb2
Joon9750 <junhyeok2166@daum.net>
2025-04-01 15:46:23 +0900

    [Add] UserChatroomFacade 인스터스 추가

--- module-application/src/main/java/org/opensource/userchatroom/facade/UserChatroomFacade.java
+++ module-application/src/main/java/org/opensource/userchatroom/facade/UserChatroomFacade.java
@@ -1,6 +1,8 @@
 package org.opensource.userchatroom.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.opensource.chatroom.facade.ChatroomFacade;
+import org.opensource.user.domain.User;
 import org.opensource.userchatroom.domain.UserChatroom;
 import org.opensource.userchatroom.port.in.usecase.UserChatroomQueryUsecase;
 import org.opensource.userchatroom.port.in.usecase.UserChatroomUsecase;
@@ -12,9 +14,11 @@ public class UserChatroomFacade {
 
     private final UserChatroomQueryUsecase userChatroomQueryUsecase;
     private final UserChatroomUsecase userChatroomUsecase;
+    private final ChatroomFacade chatroomFacade;
+//    private final UserFacade userFacade;
 
 //    public UserChatroom findUserInChatroom(Long userId, Long chatroomId) {
-//
+//        User user =
 //    }
 
 }
