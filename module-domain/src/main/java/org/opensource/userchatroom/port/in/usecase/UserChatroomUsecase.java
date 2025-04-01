fabde939ce643ca341e38cfdbb92f0f3c0cee8ba
Joon9750 <junhyeok2166@daum.net>
2025-04-01 16:07:07 +0900

    [Add] UserChatroomFacade 구현

--- module-domain/src/main/java/org/opensource/userchatroom/port/in/usecase/UserChatroomUsecase.java
+++ module-domain/src/main/java/org/opensource/userchatroom/port/in/usecase/UserChatroomUsecase.java
@@ -2,17 +2,16 @@ package org.opensource.userchatroom.port.in.usecase;
 
 import org.opensource.chatroom.domain.Chatroom;
 import org.opensource.user.domain.User;
-import org.opensource.userchatroom.port.in.command.JoinUserInChatroomCommand;
 
 public interface UserChatroomUsecase {
 
-    Long joinUserInChatroom(JoinUserInChatroomCommand command);
+    Long joinUserInChatroom(User user, Chatroom chatroom, Boolean isOnline);
 
     void deleteUserAtChatRoomByChatRoomId(User user, Chatroom chatroom);
 
     void deleteAllUsersWhenChatRoomDeleted(Chatroom chatroom);
 
-    void deleteAllChatroomsWhenUserDeleted(User user);
+    void deleteAllChatRoomsWhenUserDeleted(User user);
 
     Boolean doesUserExistInChatRoom(User user, Chatroom chatroom);
 }
