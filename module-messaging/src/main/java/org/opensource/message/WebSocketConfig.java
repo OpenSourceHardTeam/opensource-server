d6cbbb5d77759b938effabfebe4280d7c57b4537
Joon9750 <junhyeok2166@daum.net>
2025-04-03 17:37:30 +0900

    [Add] Websocket 사용하여 채팅 기능 구현:

---
+++ module-messaging/src/main/java/org/opensource/message/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package org.opensource.message;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(signalingSocketHandler(), "/ws-real-chat")
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public WebSocketHandler signalingSocketHandler() {
+        return new WebSocketHandler();
+    }
+}
