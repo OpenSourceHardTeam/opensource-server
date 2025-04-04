e09125a413d5ec0f6a47f12c0ddd422aeb498f80
SSUN <rlatjsghks4647@naver.com>
2025-04-04 23:16:13 +0900

    [FIX] user findbyemail Optional<User>로 변경 #43

--- module-infra/persistence-database/src/main/java/org/opensource/user/repository/UserJpaRepository.java
+++ module-infra/persistence-database/src/main/java/org/opensource/user/repository/UserJpaRepository.java
@@ -4,10 +4,12 @@ import org.opensource.user.domain.User;
 import org.opensource.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
 
     Boolean existsByName(String name);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
