79d73beed2741019f5623992aef2046290967976
SSUN <rlatjsghks4647@naver.com>
2025-04-04 23:00:06 +0900

    [ADD] User Success, Error Type 추가 #43

--- module-common/src/main/java/type/user/UserErrorType.java
+++ module-common/src/main/java/type/user/UserErrorType.java
@@ -8,7 +8,10 @@ public enum UserErrorType implements ErrorType {
     EMAIL_AUTH_TIMEOUT_ERROR(400, "인증 시간을 초과하였습니다. 다시 시도해주세요"),
     EMAIL_ALREADY_EXISTS(400, "이미 등록된 이메일입니다."),
     NAME_ALREADY_EXISTS(400, "이미 존재하는 이름입니다."),
-    SIGN_UP_ERROR(500, "회원가입에 실패하였습니다.")
+    SIGN_UP_ERROR(500, "회원가입에 실패하였습니다."),
+    EMAIL_NOT_EXIST(400, "등록된 이메일이 없습니다."),
+    TOKEN_TIME_EXPIRED_ERROR(401, "토큰 기간이 만료되었습니다."),
+    USER_PASSWORD_NOT_MATCH(401, "비밀번호를 잘못 입력하였습니다.")
     ;
 
     private final int code;
