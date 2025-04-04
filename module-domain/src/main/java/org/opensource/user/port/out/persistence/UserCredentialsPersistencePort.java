d07a9ab3971c6d369dafb581d2a56e43638f1915
SSUN <rlatjsghks4647@naver.com>
2025-04-04 22:55:04 +0900

    [FEAT] UserCredentials findByUserEmail 구현 #43

--- module-domain/src/main/java/org/opensource/user/port/out/persistence/UserCredentialsPersistencePort.java
+++ module-domain/src/main/java/org/opensource/user/port/out/persistence/UserCredentialsPersistencePort.java
@@ -2,6 +2,10 @@ package org.opensource.user.port.out.persistence;
 
 import org.opensource.user.domain.UserCredentials;
 
+import java.util.Optional;
+
 public interface UserCredentialsPersistencePort {
     void save(UserCredentials userCredentials);
+
+    Optional<UserCredentials> findByUserEmail(String email);
 }
