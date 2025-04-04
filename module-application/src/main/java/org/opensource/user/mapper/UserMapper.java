395ee0683b2ac1374adb84b0c82b17ba6b0dd864
SSUN <rlatjsghks4647@naver.com>
2025-04-04 23:00:51 +0900

    [FEAT] signInMapper 추가 #43

--- module-application/src/main/java/org/opensource/user/mapper/UserMapper.java
+++ module-application/src/main/java/org/opensource/user/mapper/UserMapper.java
@@ -1,11 +1,18 @@
 package org.opensource.user.mapper;
 
 import org.opensource.user.port.in.command.UserCreateCommand;
+import org.opensource.user.port.in.command.UserSignInCommand;
 
 public class UserMapper {
     public static UserCreateCommand toUserCreateCommand(
             String name, String email, String password
     ) {
         return new UserCreateCommand(name, email, password);
+    }
+
+    public static UserSignInCommand toUserSignInCommand(
+            String email, String password
+    ) {
+        return new UserSignInCommand(email, password);
     }
 }
