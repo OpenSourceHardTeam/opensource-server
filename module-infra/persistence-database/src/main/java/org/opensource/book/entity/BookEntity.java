8b8db11bf3c0724a5b862ca8c63fd2119dd84099
SSUN <rlatjsghks4647@naver.com>
2025-04-10 23:27:32 +0900

    [FEAT] Book entity 추가 #58

--- module-infra/persistence-database/src/main/java/org/opensource/book/entity/BookEntity.java
+++ module-infra/persistence-database/src/main/java/org/opensource/book/entity/BookEntity.java
@@ -13,23 +13,74 @@ public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "book_id")
-    private Long id;
+    private Long bookId;
+
+    @Column(name = "book_image_url")
+    private String bookImageUrl;
+
+    @Column(name = "book_title")
+    private String bookTitle;
+
+    @Column(name = "book_author")
+    private String bookAuthor;
+
+    @Column(name = "book_description")
+    private String bookDescription;
+
+    @Column(name = "publisher_name")
+    private String publisherName;
+
+    @Column(name = "publish_date")
+    private String publishDate;
+
+    @Column(name = "publisher_review")
+    private String publisherReview;
 
     @Builder
-    public BookEntity(Long id) {
-        this.id = id;
+    public BookEntity(
+        Long bookId,
+        String bookImageUrl,
+        String bookTitle,
+        String bookAuthor,
+        String bookDescription,
+        String publisherName,
+        String publishDate,
+        String publisherReview
+    ) {
+        this.bookId = bookId;
+        this.bookImageUrl = bookImageUrl;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookDescription = bookDescription;
+        this.publisherName = publisherName;
+        this.publishDate = publishDate;
+        this.publisherReview = publisherReview;
     }
 
 
     public static BookEntity from(Book book) {
         return builder()
-                .id(book.getId())
+                .bookId(book.getBookId())
+                .bookImageUrl(book.getBookImageUrl())
+                .bookTitle(book.getBookTitle())
+                .bookAuthor(book.getBookAuthor())
+                .bookDescription(book.getBookDescription())
+                .publisherName(book.getPublisherName())
+                .publishDate(book.getPublishDate())
+                .publisherReview(book.getPublisherReview())
                 .build();
     }
 
     public Book toModel() {
         return Book.builder()
-                .id(id)
+                .bookId(bookId)
+                .bookImageUrl(bookImageUrl)
+                .bookTitle(bookTitle)
+                .bookAuthor(bookAuthor)
+                .bookDescription(bookDescription)
+                .publisherName(publisherName)
+                .publishDate(publishDate)
+                .publisherReview(publisherReview)
                 .build();
     }
 }
