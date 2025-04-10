7263f702dfc73a6a0953d64f752978449ea43a87
SSUN <rlatjsghks4647@naver.com>
2025-04-10 23:27:32 +0900

    [FEAT] Book entity 추가 #58

--- module-domain/src/main/java/org/opensource/book/domain/Book.java
+++ module-domain/src/main/java/org/opensource/book/domain/Book.java
@@ -5,11 +5,33 @@ import lombok.Getter;
 
 @Getter
 public class Book {
-
-    private Long id;
+    private Long bookId;
+    private String bookImageUrl;
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookDescription;
+    private String publisherName;
+    private String publishDate;
+    private String publisherReview;
 
     @Builder
-    public Book(Long id) {
-        this.id = id;
+    public Book(
+        Long bookId,
+        String bookImageUrl,
+        String bookTitle,
+        String bookAuthor,
+        String bookDescription,
+        String publisherName,
+        String publishDate,
+        String publisherReview
+    ) {
+        this.bookId = bookId;
+        this.bookImageUrl = bookImageUrl;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookDescription = bookDescription;
+        this.publisherName = publisherName;
+        this.publishDate = publishDate;
+        this.publisherReview = publisherReview;
     }
 }
