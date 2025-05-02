1b36f2e0ee4c9423aab6ade8fd5eefe460e21a79
SSUN <rlatjsghks4647@naver.com>
2025-05-03 03:01:45 +0900

    [ADD] BookFacade 추가 #58

---
+++ module-application/src/main/java/org/opensource/book/facade/BookFacade.java
@@ -0,0 +1,28 @@
+package org.opensource.book.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.opensource.book.domain.Book;
+import org.opensource.book.dto.BookResponseDto;
+import org.opensource.book.port.in.usecase.BookUsecase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BookFacade {
+    private final BookUsecase bookUsecase;
+
+    public List<BookResponseDto> getAllBooks() {
+        return bookUsecase.findAllBooks()
+                .stream()
+                .map(BookResponseDto::of)
+                .toList();
+    }
+
+    public BookResponseDto getBook(Long id) {
+        return BookResponseDto.of(bookUsecase.findBookById(id));
+    }
+}
