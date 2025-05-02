60197847a19bfc236810571ec8dfbafcb8347194
SSUN <rlatjsghks4647@naver.com>
2025-05-03 03:00:05 +0900

    [FEAT] BookRepository 기능 추가 #58

--- module-infra/persistence-database/src/main/java/org/opensource/book/repository/BookRepository.java
+++ module-infra/persistence-database/src/main/java/org/opensource/book/repository/BookRepository.java
@@ -1,4 +1,29 @@
 package org.opensource.book.repository;
 
-public class BookRepository {
+import lombok.RequiredArgsConstructor;
+import org.opensource.book.domain.Book;
+import org.opensource.book.entity.BookEntity;
+import org.opensource.book.port.out.BookPort;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepository implements BookPort {
+    private final BookJpaRepository repository;
+
+    @Override
+    public List<Book> getAllBooks() {
+        return repository.findAllByOrderByBookRankAsc()
+                .stream()
+                .map(BookEntity::toModel)
+                .toList();
+    }
+
+    @Override
+    public Optional<Book> getBookById(Long id) {
+        return repository.findById(id).map(BookEntity::toModel);
+    }
 }
