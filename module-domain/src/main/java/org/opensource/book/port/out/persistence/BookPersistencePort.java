f02178361dcadffbe30d0b73b1347b032100c406
SSUN <rlatjsghks4647@naver.com>
2025-05-03 21:35:28 +0900

    [CHORE] BookPort 이름 변경 #58

--- module-domain/src/main/java/org/opensource/book/port/out/persistence/BookPersistencePort.java
+++ module-domain/src/main/java/org/opensource/book/port/out/persistence/BookPersistencePort.java
@@ -5,7 +5,7 @@ import org.opensource.book.domain.Book;
 import java.util.List;
 import java.util.Optional;
 
-public interface BookPort {
+public interface BookPersistencePort {
     List<Book> getAllBooks();
 
     Optional<Book> getBookById(Long id);
