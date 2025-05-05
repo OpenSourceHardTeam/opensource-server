58f4c329ecc251046e9f15b944e5b8aac0588476
SSUN <rlatjsghks4647@naver.com>
2025-05-06 02:17:56 +0900

    [ADD] Vote, VoteUser port 추가 #59

---
+++ module-domain/src/main/java/org/opensource/vote/port/out/persistence/VotePersistencePort.java
@@ -0,0 +1,17 @@
+package org.opensource.vote.port.out.persistence;
+
+import org.opensource.vote.domain.Vote;
+import org.opensource.vote.port.in.command.AddVoteCommand;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VotePersistencePort {
+    void vote(Vote vote);
+
+    void addVote(Vote vote);
+
+    List<Vote> findAllVoteByBookId(Long bookId);
+
+    Optional<Vote> findByVoteId(Long voteId);
+}
---
+++ module-domain/src/main/java/org/opensource/vote/port/out/persistence/VoteUserPersistencePort.java
@@ -0,0 +1,15 @@
+package org.opensource.vote.port.out.persistence;
+
+import org.opensource.vote.domain.VoteUser;
+
+import java.util.Optional;
+
+public interface VoteUserPersistencePort {
+    void addVoteUser(VoteUser voteUser);
+
+    void changeAnswer(VoteUser voteUser);
+
+    Optional<VoteUser> findUserVotedByUserIdAndVoteId(Long userId, Long voteId);
+
+    Boolean isUserVotedByUserIdAndVoteId(Long userId, Long voteId);
+}
