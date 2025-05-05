8c8a346f4f0b30aaa8d07095a64aaa1b43a93d86
SSUN <rlatjsghks4647@naver.com>
2025-05-06 02:21:15 +0900

    [ADD] VoteController 추가 #59

---
+++ module-application/src/main/java/org/opensource/vote/api/VoteApi.java
@@ -0,0 +1,18 @@
+package org.opensource.vote.api;
+
+import dto.response.ApiResponse;
+import org.opensource.vote.dto.request.AddVoteRequestDto;
+import org.opensource.vote.dto.request.VoteRequestDto;
+import org.opensource.vote.dto.response.GetVoteResponseDto;
+
+import java.util.List;
+
+public interface VoteApi {
+    ApiResponse addVote(AddVoteRequestDto addVoteRequestDto);
+
+    ApiResponse vote(Long userId, VoteRequestDto voteRequestDto);
+
+    ApiResponse<List<GetVoteResponseDto>> getAllVotes(Long bookId);
+
+    ApiResponse<Boolean> getVoteAnswered(Long userId, Long voteId);
+}
