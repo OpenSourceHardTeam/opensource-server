67280d5506b6810a1c57e7a17a80ebe1ac36dd08
SSUN <rlatjsghks4647@naver.com>
2025-05-06 02:20:15 +0900

    [ADD] GetVoteResponseDto 추가 #59

---
+++ module-application/src/main/java/org/opensource/vote/dto/response/GetVoteResponseDto.java
@@ -0,0 +1,28 @@
+package org.opensource.vote.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.opensource.vote.domain.Vote;
+
+@Getter
+@AllArgsConstructor
+public class GetVoteResponseDto {
+    private Long voteId;
+    private Long bookId;
+    private String title;
+    private String content;
+    private Integer agreeCount;
+    private Integer disagreeCount;
+    private Integer voteCount;
+
+    public static GetVoteResponseDto of(Vote vote) {
+        return new GetVoteResponseDto(
+                vote.getId(),
+                vote.getBook().getBookId(),
+                vote.getTitle(),
+                vote.getContent(),
+                vote.getAgreeCount(),
+                vote.getDisagreeCount(),
+                vote.getVoteCount());
+    }
+}
