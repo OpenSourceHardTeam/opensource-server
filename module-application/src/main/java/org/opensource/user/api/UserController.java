52d84d019d12be0dc275ac46040fee5f261d8ecc
SSUN <rlatjsghks4647@naver.com>
2025-05-14 12:11:59 +0900

    [ADD] Swagger Body 설명 추가 #66

--- module-application/src/main/java/org/opensource/user/api/UserController.java
+++ module-application/src/main/java/org/opensource/user/api/UserController.java
@@ -24,6 +24,8 @@ public class UserController implements UserApi {
     @PostMapping("/signup")
     @Operation(summary = "회원가입 API", description = "유저 회원가입")
     public ApiResponse<SignUpResponseDto> signUp(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "닉네임은 2~10자여야합니다" +
+                    "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 5자 ~ 20자의 비밀번호여야 합니다")
             @RequestBody @Valid final SignUpRequestDto request
     ) {
         return ApiResponse.success(UserSuccessType.SIGN_UP_SUCCESS, userFacade.signUp(request.getName(), request.getEmail(), request.getPassword()));
