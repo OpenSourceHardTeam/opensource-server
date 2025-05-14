68475c7ade75b0ffa9c620053f244b2e698988c6
SSUN <rlatjsghks4647@naver.com>
2025-05-14 12:00:22 +0900

    [FIX] 회원가입 형식 변경 #66

--- module-application/src/main/java/org/opensource/user/dto/request/SignUpRequestDto.java
+++ module-application/src/main/java/org/opensource/user/dto/request/SignUpRequestDto.java
@@ -11,7 +11,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SignUpRequestDto {
     @NotBlank
-    @Pattern(regexp = "^[가-힣a-zA-Z]{2,10}$", message = "닉네임 형식에 맞지 않습니다")
+    @Pattern(
+            regexp = "^[가-힣a-zA-Z0-9]{2,10}$",
+            message = "닉네임은 2~10자여야합니다")
     private String name;
 
     @NotBlank
@@ -20,8 +22,8 @@ public class SignUpRequestDto {
 
     @NotNull
     @Pattern(
-            regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다"
+            regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{5,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 5자 ~ 20자의 비밀번호여야 합니다"
     )
     private String password;
 }
