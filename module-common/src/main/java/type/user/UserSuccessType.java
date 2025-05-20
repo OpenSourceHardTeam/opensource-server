1a14e5fa50a290b7820637a8faec2700f5d19983
SSUN <rlatjsghks4647@naver.com>
2025-05-20 20:45:49 +0900

    [ADD] UserSuccessType 추가 #79

--- module-common/src/main/java/type/user/UserSuccessType.java
+++ module-common/src/main/java/type/user/UserSuccessType.java
@@ -8,7 +8,9 @@ public enum UserSuccessType implements SuccessType {
     SIGN_UP_SUCCESS(200, "회원가입이 완료되었습니다."),
     EMAIL_CAN_USE(200, "사용 가능한 이메일입니다."),
     NAME_CAN_USE(200, "사용 가능한 이름입니다."),
-    SIGN_IN_SUCCESS(200, "로그인에 성공하였습니다.")
+    SIGN_IN_SUCCESS(200, "로그인에 성공하였습니다."),
+    CHANGE_USER_NAME_SUCCESS(200, "사용자 이름을 변경하였습니다."),
+    CHANGE_USER_PASSWORD_SUCCESS(200, "사용자 비밀번호를 변경하였습니다.")
     ;
 
     private final int code;
