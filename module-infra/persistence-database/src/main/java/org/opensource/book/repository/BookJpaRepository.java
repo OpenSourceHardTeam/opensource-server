442176f89bdcfe82da5ea5514ea1590fd8f94324
SSUN <rlatjsghks4647@naver.com>
2025-06-06 17:34:07 +0900

    [FEAT] 제목으로 책 검색 Repository 추가 #93

--- module-infra/persistence-database/src/main/java/org/opensource/book/repository/BookJpaRepository.java
+++ module-infra/persistence-database/src/main/java/org/opensource/book/repository/BookJpaRepository.java
@@ -2,10 +2,15 @@ package org.opensource.book.repository;
 
 import org.opensource.book.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookJpaRepository extends JpaRepository<BookEntity, Long> {
     List<BookEntity> findAllByOrderByBookRankAsc();
+
+    @Query("SELECT b FROM BookEntity b WHERE REPLACE(LOWER(b.bookTitle), ' ', '') LIKE LOWER(CONCAT('%', REPLACE(:keyword, ' ', ''), '%'))")
+    List<BookEntity> searchBookEntityByBookTitle(@Param("keyword") String keyword);
 }
